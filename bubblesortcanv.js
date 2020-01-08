01156398f400d6b12e547e1870f624799256dd89
andrerim <andreasrim@gmail.com>
2020-01-08 20:58:17 +0100

    improved visualization of bubble sort to actually resemble bubble sort, and extraced code to main.js to make it easier to implement different sorts

--- bubblesortcanv.js
+++ bubblesortcanv.js
@@ -1,59 +1,26 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext("2d");
 
-const canvW = canvas.width;
-const canvH = canvas.height;
-const lineW = 5;   
-const amountOfElements = Math.ceil(canvW/lineW);
+function bubbleSort(elements, elLength) {
+  let swapped = false;
 
-
-var elements = [];
-
-let randInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-for (let i = 0; i < amountOfElements; i++){
-    elements.push(randInt(0, canvH));
-};
-
-function clearCanv(){
-    ctx.clearRect(0, 0, canvW, canvH);
-}
-
-function drawEl(element, index){
-    ctx.fillRect(index*lineW, canvH - element, lineW, canvH);
-};
-
-function drawCanv(){
-    elements.forEach((el, index) => {
-        drawEl(el, index);
-    });
-}
-
-drawCanv();
-
-async function bubbleSort(){
-    let sorted = false;
-    while (!sorted){
-        for (let i = 0; i < elements.length - 1; i++){
-            if (elements[i] > elements[i+1]){
-                console.log(elements);
-                let tempEl = elements[i];
-                elements[i] = elements[i+1];
-                elements[i+1] = tempEl;
-                clearCanv();
-                drawCanv();
-                sorted = false;
-                console.log(elements)
-                break;
-            } 
-            sorted = true; 
-        }
-        await new Promise(r => setTimeout(r, 10));
+  function iterate(i) {
+    if (elements[i] > elements[i + 1]) {
+      let tempEl = elements[i];
+      elements[i] = elements[i + 1];
+      elements[i + 1] = tempEl;
+      clearCanv(ctx);
+      drawCanv(ctx, elements);
+      swapped = true;
     }
-};
 
-bubbleSort();
-
+    if (elLength > i) {
+      setTimeout(() => iterate(i + 1), sortSpeed);
+    } else if (swapped) {
+      setTimeout(() => bubbleSort(elements, elLength - 1), sortSpeed);
+    } else {
+      return;
+    }
+  }
+  iterate(0);
+}
 
---
+++ main.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("canvas");
+
+
+
+
+const canvW = canvas.width;
+const canvH = canvas.height;
+const lineW = 5;
+const amountOfElements = Math.ceil(canvW / lineW);
+const sortSpeed = 1;
+
+function clearCanv(ctx){
+    ctx.clearRect(0, 0, canvW, canvH);
+}
+
+function drawEl(ctx, element, index){
+    ctx.fillRect(index*lineW, canvH - element, lineW, canvH);
+};
+
+function drawCanv(ctx, elements){
+    elements.forEach((el, index) => {
+        drawEl(ctx, el, index);
+    });
+}
+
+
+var elements = [];
+
+let randInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+for (let i = 0; i < amountOfElements; i++){
+    elements.push(randInt(0, canvH));
+};
+
+
+bubbleSort(elements, elements.length);
